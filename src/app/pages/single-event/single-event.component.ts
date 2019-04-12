7bf99f7f10f97a679db23d0dad408dd785970fd7
Faraz Sarwar <faraz@sudofy.com>
2019-04-13 01:44:23 +0500

    Event and single event API integrated

--- src/app/pages/single-event/single-event.component.ts
+++ src/app/pages/single-event/single-event.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EndPointsService } from '../../services/end-points.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-event',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./single-event.component.scss']
 })
 export class SingleEventComponent implements OnInit {
-
-  constructor() { }
+  event;
+  constructor(private apiService: EndPointsService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.apiService.getEvent(this.activatedRoute.snapshot.params.id).subscribe((res: any) => {
+      console.log(res);
+      this.event = res.data;
+    }, err => {
+      console.log(err);
+    });
   }
 
 }
